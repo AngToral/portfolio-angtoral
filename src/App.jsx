4af872544f5c5937665fc2c87069ca551fb19e3a
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-01-07 17:18:18 +0100

    corrijo fondo

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className='proyecto lg:px-6 items-center flex pt-4 pb-6 place-content-center bg-ang-100'>
+      <div className='proyecto lg:px-6 items-center flex pt-4 pb-6 place-content-center'>
         <div className='flex flex-col items-center justify-center lg:w-1/2'>
           <ScrollAnimation animateIn="animate__animated animate__backInLeft" animateOnce={true}>
             <video className="rounded-lg" controls>
@@ -152,7 +152,7 @@ function App() {
             </ScrollAnimation>
           </div>
         </div>
-        <div className='proyecto flex flex-col items-center justify-center lg:w-1/2 ml-6'>
+        <div className='flex flex-col items-center justify-center lg:w-1/2 ml-6'>
           <ScrollAnimation animateIn="animate__animated animate__backInRight" animateOnce={true}>
             <img className='max-h-72 w-auto mr-4' src='../mariana.avif' />
           </ScrollAnimation>
